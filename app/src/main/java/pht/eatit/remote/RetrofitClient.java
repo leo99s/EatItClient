5da1d068895b2e738437c47bf10d46bc32aebbac
Hoàng Tâm <phanhoangtam.pht@gmail.com>
2018-02-21 16:30:23 +0700

    Fix error of shipping address

--- app/src/main/java/pht/eatit/remote/RetrofitClient.java
+++ app/src/main/java/pht/eatit/remote/RetrofitClient.java
@@ -6,28 +6,17 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
 
-    private static Retrofit retrofitFCM = null;
-    private static Retrofit retrofitMap = null;
-
     public static Retrofit getFCMClient(String baseUrl){
-        if(retrofitFCM == null){
-            retrofitFCM = new Retrofit.Builder()
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-
-        return retrofitFCM;
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 
     public static Retrofit getMapClient(String baseUrl){
-        if(retrofitMap == null){
-            retrofitMap = new Retrofit.Builder()
-                    .baseUrl(baseUrl)
-                    .addConverterFactory(ScalarsConverterFactory.create())
-                    .build();
-        }
-
-        return retrofitMap;
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
     }
 } 
